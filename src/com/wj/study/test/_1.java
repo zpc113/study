1f9867af5ab7b8af7152f08d1ce6ef1b60c82dcd
zpc <228905514@qq.com>
2018-02-09 01:15:16 +0800

    给你出了一道题

---
+++ src/com/wj/study/test/_1.java
@@ -0,0 +1,29 @@
+package com.wj.study.test;
+
+import java.util.Scanner;
+
+/**
+ * 给定一个整数数组，找出其中两个数满足相加等于你指定的目标数字。 
+　　要求：这个函数twoSum必须要返回能够相加等于目标数字的两个数的索引，且index1必须要小于index2。
+       请注意一点，你返回的结果（包括index1和index2）都不是基于0开始的。你可以假设每一个输入肯定只有一个结果。 
+ * @author 和谐社会人人有责
+ *
+ */
+public class _1 {
+
+	public static int[] arr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9};
+	
+	public static void twoSum(int num) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		twoSum(num);
+		
+		
+	}
+	
+}
