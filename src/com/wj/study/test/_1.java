e36dbd4deb43c33eefeb026c4175b48e7424acf0
wj <827946303@qq.com>
2018-02-09 23:02:05 +0800

    作业

--- src/com/wj/study/test/_1.java
+++ src/com/wj/study/test/_1.java
@@ -1,5 +1,6 @@
 package com.wj.study.test;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -14,15 +15,33 @@ public class _1 {
 	public static int[] arr = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9};
 	
 	public static void twoSum(int num) {
-		
+		int sum = 1;
+		for (int i = 0 ; i < arr.length ; i ++ ){
+			int num1 = arr[i];
+			int num2 = num - num1;
+			sum++;
+			if (num1 < num2 ){
+				for (int j = 0 ; j < arr.length ; j++){
+				    if (num2 == arr[j]){
+				    	System.out.println(j);
+				    	int index1 = i ;
+						int index2 = j ;
+						System.out.println("两个索引位置分别是： index1 ," + index1 + "\t index2 ," + index2);
+				    }
+				}
+		    }   
+		}
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println("请输入一个正整数：");
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
-		twoSum(num);
-		
+		if (num > 17 || num <= 2){
+			System.out.println("输入错误！数组中的数字无法满足相加等于" + num );
+		} else {
+		    twoSum(num);
+		}
 		
 	}
 	
