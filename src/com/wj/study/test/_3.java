d759210678f32bf5cddcc0edde29aac08699d6fb
zpc <228905514@qq.com>
2018-02-10 20:59:30 +0800

    第三题

--- src/com/wj/study/test/_3.java
+++ src/com/wj/study/test/_3.java
@@ -1,9 +1,5 @@
 package com.wj.study.test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * 给定一个字符串，找字符中的最大非重复子串 
  * For example, the longest substring without repeating letters for “abcabcbbcdefbbdefbbghdjsk” is “bghdjsk”,  
