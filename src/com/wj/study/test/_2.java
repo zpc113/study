2e4a0c7a0f3598aa65ceb116350856e2f0b1ae31
wj <827946303@qq.com>
2018-02-13 20:48:02 +0800

    1是昨天写的，2

--- src/com/wj/study/test/_2.java
+++ src/com/wj/study/test/_2.java
@@ -25,31 +25,72 @@ public class _2 {
 		arr2.add(4);
 		// 把这两个list里面的数字转换为十进制的数，如 arr1 -> 342  arr2 -> 465
 		// 然后再继续
-		int sum3 = test1(arr1) + test1(arr2);
+		int sum3 = test1(arr1) + test1(arr2);//1
 		List<Integer> arr3 = new ArrayList<Integer>();
-		String temp = sum3 + "";
-		System.out.println(temp);
-		int length = temp.length();
-        for (int i = 0 ; i < temp.length() ;i++){
-        	String j = temp.charAt(temp.length() - 1 - i) + "";
-        	int k = Integer.parseInt(j);
-        	System.out.println(j);
-			arr3.add(k);
+		int a = 0;
+		for (int i = 0 ; sum3 >= (Math.pow(10, i));i++){
+			int b = (int) (sum3/(Math.pow(10, i)));
+			int c = (int) (b/10) ;
+			int d = b - c*10 ;
+			arr3.add(d);
 		}
-        System.out.println(arr3);
+		System.out.println(arr3);
 		
 		
+		
+		
+		
+		int num1 = 0;//2
+		int num2 = 0;
+		for (int i = 0 ; i < arr1.size() ;i++){
+			int temp = arr1.get(i);
+			for (int j = 0 ;j < i ;j++ ){
+				temp *= 10;
+			}
+			num1 += temp;
+		}
+		num1 += arr2.get(0);
+		for (int i = 0 ; i < arr2.size() ;i++){
+			int temp = arr2.get(i);
+			for (int j = 0 ;j < i ;j++ ){
+				temp *= 10;
+			}
+			num2 += temp;
+		}
+		num2 += arr2.get(0);
+		
+		//int j = sum3/100;
+		//int k = (sum3-j*100)/10;
+		//int l = sum3-j*100-k*10;
+		//arr3.add(l);
+		//arr3.add(k);
+		//arr3.add(j);
+        //System.out.println(arr3);
+        //for (int i = 0 ; i < temp.length() ;i++){//3
+        //	  String j = temp.charAt(temp.length() - 1 - i) + "";
+        // 	  int k = Integer.parseInt(j);
+        //	  System.out.println(j);
+        //	  arr3.add(k);
+      	//}
 	}
 	
 	public static int test1(List<Integer> arr){
-		int size = arr.size();
-		String num = "";
-		for (int i = size -1 ,j = size - 1 - i; i >= 0 ; i--){
-			num = num + arr.get(i);
+		//int size = arr.size();//3
+		//String num = "";
+		//for (int i = size -1 ,j = size - 1 - i; i >= 0 ; i--){
+		//	num = num + arr.get(i);
+		//}
+		//int sum = Integer.parseInt(num);
+		//System.out.println(sum);
+		//return sum;
+		int a = 0;//1
+		for (int i = 0 ; i < arr.size();i++){
+			int a1 = (int) (arr.get(i)*(Math.pow(10, i)));
+			a = a + a1;
 		}
-		int sum = Integer.parseInt(num);
-		System.out.println(sum);
-		return sum;
+		System.out.println(a);
+		return a;
 	}
+		
 
 }
