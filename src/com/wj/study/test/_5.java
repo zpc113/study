18857270b7927126bebab40a610cd4182e26461b
zpc <228905514@qq.com>
2018-02-22 18:49:28 +0800

    第五题

---
+++ src/com/wj/study/test/_5.java
@@ -0,0 +1,20 @@
+package com.wj.study.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 给定一个字符串S，找出它的最大的回文子串，你可以假设字符串中存在唯一的最长回文子串 
+ *
+ */
+public class _5 {
+	
+	public static void main(String[] args) {
+		String s = "abcbaabcdefedcbaabcdcba";
+		
+	}
+	
+}
