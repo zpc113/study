abf13404ab8771856194c26645fb6a555cd6fbff
zpc <228905514@qq.com>
2018-02-25 22:26:15 +0800

    第六题

---
+++ src/com/wj/study/test/_6.java
@@ -0,0 +1,24 @@
+package com.wj.study.test;
+
+/**
+ * 输入一个字符串和指定的行数，将字符以Z字型输出。
+ * @author 和谐社会人人有责
+ *
+ */
+public class _6 {
+	public static void main(String[] args) {
+		String str = "PAYPALISHIRING";
+		/**
+		 * P A H N 
+		　　APLSIIG 
+		　　Y I R 
+		
+		   P   I   N
+		   A  LS  IG
+		   Y A H R
+		   P   I
+		 */
+		
+	}
+	
+}
