d526db48f9cfdb0128d78e0ce244d4e1bf874759
zpc <228905514@qq.com>
2018-02-27 22:04:51 +0800

    又来了两道题

---
+++ src/com/wj/study/test/_14.java
@@ -0,0 +1,20 @@
+package com.wj.study.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 写一个函数找出一个字串所数组中的最长的公共前缀。
+ * @author 和谐社会人人有责
+ *
+ */
+public class _14 {
+	public static void main(String[] args) {
+		
+		String[] strArr = {"abcdef" , "abcghskg" , "acduregjsa" , "acfkdssssa" , "auerjkfdsfs" , "akugoendm"};
+		// 最长公共前缀  abc
+	}
+}
---
+++ src/com/wj/study/test/_7.java
@@ -0,0 +1,19 @@
+package com.wj.study.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Reverse digits of an integer. 
+　　Example1: x = 123, return 321 
+　　Example2: x = -123, return -321 
+       输入一个整数对其进行翻转 
+ * @author 和谐社会人人有责
+ *
+ */
+public class _7 {
+	public static void main(String[] args) {
+		int num = -123;
+		
+	}
+}
